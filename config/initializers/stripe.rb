11692798957193b772cd762e77a000fc7f95d21e
nirmalaraut <rautnirmala1999@gmail.com>
2021-01-23 10:12:02 +0530

    my initial stripe project

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,4 @@
 # set the Stripe keys in the file config/secrets.yml
 Stripe.api_key = Rails.application.secrets.stripe_api_key
+PUBLISHABLE_KEY='pk_live_51IAvQ5JjTGPzDQQ3sHntLF1ATPfxbfmz1cQUk5JWaBFqWZeCkuQbLIzTCwDhalD5RLL4qIlaa3k4LCqgn43RFjY700AVi7TEqs'
+SECRET_KEY='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'
